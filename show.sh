4273b4ec29775527ba8b2734530a38ca23da85fc
Davis Li <dengfeng.li@salesforce.com>
2017-10-26 20:44:00 -0700

    add script to open browser

--- show.sh
+++ show.sh
@@ -5,7 +5,7 @@ xhost + ${ip}
 PROCESS_NUM=$(ps -ef | grep XQuartz | grep -v "grep"| wc -l)
 
 if [ $PROCESS_NUM -gt 0 ]; then
-	docker run -e DISPLAY=${ip}:0 --network=sparkdocker_hadoopnet -v /tmp/.X11-unix:/tmp/.X11-unix jess/firefox namenode:8088
+	docker run -e DISPLAY=${ip}:0 --network=sparkdocker_hadoopnet -v /tmp/.X11-unix:/tmp/.X11-unix jess/firefox --disable-save-password-bubble --no-first-run namenode:8088 airflow:8080
 else
 	echo "Please install and start XQuartz"
 fi
